b70209a754a92b263250af8923008d91af039c09
Jinge <jinge_windows@hotmail.com>
2020-12-02 19:05:26 +1100

    删除了一部分代码，界面bug依然存在。准备删除类。

--- UWP_FileSliceAndMerge_Prism/ViewModels/TxtFile/TxtSplitViewModel.cs
+++ UWP_FileSliceAndMerge_Prism/ViewModels/TxtFile/TxtSplitViewModel.cs
@@ -17,142 +17,22 @@ namespace UWP_FileSliceAndMerge_Prism.ViewModels
 {
     public class TxtSplitViewModel : ViewModelBase
     {
-        private readonly string _settingKey = "TxtSplit_IsSaveOutputFolderAsDefault";
-        private readonly string _folderToken = "TxtSplit_OutputFolderToken";
-        private readonly int MaxSliceNumber = 999999;
 
         public ObservableCollection<TxtEntiretyInfoModel> EntiretyFiles { get; set; } =
                             new ObservableCollection<TxtEntiretyInfoModel>();
-        private ObservableCollection<TxtSliceInfoModel> _sliceFiles = new ObservableCollection<TxtSliceInfoModel>();
-        public ObservableCollection<TxtSliceInfoModel> SliceFiles
-        {
-            get { return _sliceFiles; }
-            set { SetProperty(ref _sliceFiles, value); }
-        }
-
-        public List<string> NumberList { get; set; } = new List<string>
-        {
-            "100",
-            "500",
-            "1000",
-            "5000",
-            "10000"
-        };
-
-        private int _numberSelected=100;
-        private string _numberSelectedText="100";
-        public string NumberSelectedText
-        {
-            get { return _numberSelectedText; }
-            set
-            {
-                if (_numberSelectedText != value)
-                {
-                    SetProperty(ref _numberSelectedText, value);
-                    checkSliceNumber(_numberSelectedText);
-                }
-
-            }
-        }
-        private int _numberUnitSelectedIndex;
-        public int NumberUnitSelectedIndex
-        {
-            get { return _numberUnitSelectedIndex; }
-            set
-            {
-                if (_numberUnitSelectedIndex != value)
-                {
-                    SetProperty(ref _numberUnitSelectedIndex, value);
-                    checkSliceNumber(NumberSelectedText);
-                }
-            }
-        }
-
-
-        private bool _isStarted = false;
-        public bool IsStarted
-        {
-            get { return _isStarted; }
-            set { SetProperty(ref _isStarted, value); }
-        }
-        private bool _isFinish;
-        public bool IsFinish
-        {
-            get { return _isFinish; }
-            set { SetProperty(ref _isFinish, value); }
-        }
-        private bool _isChinese = true;
-        public bool IsChinese
-        {
-            get { return _isChinese; }
-            set { SetProperty(ref _isChinese, value); }
-        }
-        private StorageFolder _outputFolder;
-        public StorageFolder OutputFolder
-        {
-            get { return _outputFolder; }
-            set
-            {
-                SetProperty(ref _outputFolder, value);
-                if (value != null)
-                {
-                    IsOutputFolderSetAsDefaultVisiable = true;
-                }
-            }
-        }
-
-        private bool _isOutputFolderSetAsDefaultVisiable = false;
-        public bool IsOutputFolderSetAsDefaultVisiable
-        {
-            get { return _isOutputFolderSetAsDefaultVisiable; }
-            set { SetProperty(ref _isOutputFolderSetAsDefaultVisiable, value); }
-        }
-
-
-        private bool _isSaveOutputFolderAsDefault;
-        public bool IsSaveOutputFolderAsDefault
-        {
-            get { return _isSaveOutputFolderAsDefault; }
-            set
-            {
-                SetProperty(ref _isSaveOutputFolderAsDefault, value);
-                MyAppSettingHelper.AppSetting.SaveAsync<bool>(_settingKey, value);
-            }
-        }
-
-        private bool _isSplitByNumber = true;
-        public bool IsSplitByNumber
-        {
-            get { return _isSplitByNumber; }
-            set { SetProperty(ref _isSplitByNumber, value); }
-        }
-        private bool _isSplitNumberWarningShow;
-        public bool IsSplitNumberWarningShow
-        {
-            get { return _isSplitNumberWarningShow; }
-            set { SetProperty(ref _isSplitNumberWarningShow, value); }
-        }
-
         public DelegateCommand SelectSourceFilesCommand { get; set; }
         public DelegateCommand ClearSourceFilesCommand { get; set; }
-        public DelegateCommand SelectOutputFolderCommand { get; set; }
-        public DelegateCommand StartSplitCommand { get; set; }
-        public DelegateCommand LaunchFolderCommand { get; set; }
 
         public TxtSplitViewModel()
         {
-            SelectSourceFilesCommand = new DelegateCommand(selectSourceFiles, () => !IsStarted)
-                .ObservesProperty(() => IsStarted);
-            ClearSourceFilesCommand = new DelegateCommand(clearUi, () => !IsStarted)
-                .ObservesProperty(() => IsStarted);
-            SelectOutputFolderCommand = new DelegateCommand(selectOutputFolder, () => !IsStarted)
-                .ObservesProperty(() => IsStarted);
+            SelectSourceFilesCommand = new DelegateCommand(async ()=>await selectSourceFiles());
+            ClearSourceFilesCommand = new DelegateCommand(clearUi);
         }
 
         /// <summary>
         /// 用户选择待处理的源文件
         /// </summary>
-        private async void selectSourceFiles()
+        private async Task selectSourceFiles()
         {
             var filesPicker = new Windows.Storage.Pickers.FileOpenPicker();
             filesPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
@@ -161,9 +41,6 @@ namespace UWP_FileSliceAndMerge_Prism.ViewModels
             if (files.Count > 0)
             {
                 await getSourceFileInfo(files);
-                preview();
-                //让该Command重新检测是否能够执行的条件
-                //StartSplitCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -173,7 +50,6 @@ namespace UWP_FileSliceAndMerge_Prism.ViewModels
         /// <returns></returns>
         private async Task getSourceFileInfo(IEnumerable<StorageFile> sourceStorageFiles)
         {
-            EntiretyFiles.Clear();
             foreach (StorageFile file in sourceStorageFiles)
             {
                 Windows.Storage.FileProperties.BasicProperties basicProperties =
@@ -182,10 +58,12 @@ namespace UWP_FileSliceAndMerge_Prism.ViewModels
                 {
                     StorageFile = file,
                     FileName = file.Name,
-                    FileSize = (long)basicProperties.Size
+                    FileSize = (long)basicProperties.Size,
+                    TxtWordCount = 100,
+                    TxtLineCount = 100,
+                    TextLength=100
                 });
             }
-            await calculateTxtInfo();
         }
 
         /// <summary>
@@ -193,76 +71,8 @@ namespace UWP_FileSliceAndMerge_Prism.ViewModels
         /// </summary>
         private void clearUi()
         {
-            IsFinish = false;
             EntiretyFiles.Clear();
-            SliceFiles.Clear();
-            //让该Command重新检测是否能够执行的条件
-            StartSplitCommand.RaiseCanExecuteChanged();
         }
 
-        private async Task calculateTxtInfo()
-        {
-            TxtInfoService txtInfoService = new TxtInfoService(EntiretyFiles, IsChinese);
-            await txtInfoService.FindWordCountAndLineCount();
-        }
-
-        /// <summary>
-        /// 用户选择输出路径
-        /// </summary>
-        private async void selectOutputFolder()
-        {
-            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
-            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
-            folderPicker.FileTypeFilter.Add("*");
-            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
-            if (folder != null)
-            {
-                //应用此时获得读写该文件夹的权限，包括子文件夹
-                Windows.Storage.AccessCache.StorageApplicationPermissions.
-                    FutureAccessList.AddOrReplace(_folderToken, folder);
-                OutputFolder = folder;
-            }
-        }
-
-        private void preview()
-        {
-            SplitPreviewService previewService = new SplitPreviewService(EntiretyFiles, 1, 1);
-            List<TxtSliceInfoModel> resultList=previewService.GetPreviewByLineCount(_numberSelected);
-            SliceFiles = new ObservableCollection<TxtSliceInfoModel>(resultList);
-        }
-
-        /// <summary>
-        /// 检测用户输入的切片数量是否合法
-        /// </summary>
-        private async void checkSliceNumber(string inputText)
-        {
-            Debug.WriteLine("执行输入检验 " + inputText);
-            if (Int32.TryParse(inputText, out int inputNumber) && inputNumber > 0)
-            {
-                //int sliceNumber = Int32.Parse(inputText);
-                if (inputNumber > MaxSliceNumber)
-                {
-                    await showOverLimitWarning(inputNumber);
-                    return;
-                }
-                _numberSelected = inputNumber;
-                preview();
-            }
-            else
-            {
-                IsSplitNumberWarningShow = true;
-            }
-        }
-
-        /// <summary>
-        /// 当用户的设定值超过最大切片数量限制，弹出警告。
-        /// </summary>
-        /// <returns></returns>
-        private async Task showOverLimitWarning(long outputFileNumber)
-        {
-            BinarySplitSettingWarningDialog dialog = new BinarySplitSettingWarningDialog(outputFileNumber);
-            await dialog.ShowAsync();
-            //Debug.WriteLine("弹出警告窗口");
-        }
     }
 }
